518f5965206b3b475729d7c204eadf0fa7033b61
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-11 11:50:40 -1000

    Getting stated with React Redux

--- reactRedux.js
+++ reactRedux.js
@@ -1,1 +1,15 @@
 // Getting started with React Redux
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      input: "",
+      messages: []
+    }
+  }
+  render() {
+    return <div />
+  }
+};
+
+// Manage state locally first

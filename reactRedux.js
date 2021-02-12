0ff084641eb6327699887701081d200f3b831842
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-12 10:01:16 -1000

    Map dispatch to props

--- reactRedux.js
+++ reactRedux.js
@@ -94,3 +94,18 @@ const mapStateToProps = (state) => {
 };
 
 // Map dispatch to props
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message))
+    }
+  }
+};
+
+// 
